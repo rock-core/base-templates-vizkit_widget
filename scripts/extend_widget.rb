d2b67c3fd490ef5a0cb8f06c8605cd59afd08af8
Alexander Duda <Alexander.Duda@dfki.de>
2011-03-21 23:27:24 +0100

    add extend widget script

---
+++ scripts/extend_widget.rb
@@ -0,0 +1,21 @@
+#extend the c++ widget ClassName with ruby methods
+Vizkit::UiLoader::extend_cplusplus_widget_class "ClassName" do
+
+  #test method
+  #you can delete it 
+  def ruby_method()
+    setText("bla")
+  end
+
+#callback function to display a sample
+#  def update(sample,port_name)
+#    puts "got new sample"
+#  end
+
+end
+
+#If you want to use the widget to display samples add the following line 
+#and replace /base/samples/frame/Frame with the sample type you want to display. 
+#After that you can call Viszkit.display task.port to display output ports those
+#samples types are of that type.
+#Vizkit::UiLoader.register_widget_for("ClassName","/base/samples/frame/Frame",:update)
--- scripts/run.rb
+++ scripts/run.rb
@@ -1,7 +1,8 @@
 require 'vizkit'
+require 'extend_widget'
 
 #try to load widget
 widget = Vizkit.default_loader.ClassName
 widget.show
-widget.setText("ClassName: Hello Rock")
+widget.ruby_method
 Vizkit.exec
